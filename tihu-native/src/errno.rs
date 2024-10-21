4467eaece2811eaab6ec0cc938101b0028acfcfc
omega <omega.xiao@loosegoose.onaliyun.com>
2024-10-22 07:39:22 +0800

    add some predefined errors

---
+++ tihu-native/src/errno.rs
@@ -0,0 +1,145 @@
+use tihu::LightString;
+
+#[derive(thiserror::Error, Debug)]
+pub enum ErrNo {
+    #[error("{0}")]
+    CommonError(LightString),
+    #[error("{0}")]
+    Other(#[from] anyhow::Error),
+    #[error("配置错误，{0}")]
+    ConfigError(LightString),
+    #[error("没有可用的{0}服务")]
+    NoService(LightString),
+    #[error("{0}服务忙")]
+    ServiceBusy(LightString),
+    #[error("暂停服务")]
+    ServicePaused,
+    #[error("接口不存在")]
+    NoSuchApi,
+    #[error("序列化数据失败,{0}")]
+    SerializeError(serde_json::Error),
+    #[error("反序列化数据失败,{0}")]
+    DeserializeError(serde_json::Error),
+    #[error("调用远程接口失败,{0}")]
+    ApiError(anyhow::Error),
+    #[error("只支持UTF-8格式的数据")]
+    Utf8Only,
+    #[error("参数格式不正确")]
+    ParamFormatError,
+    #[error("参数无效,{0}")]
+    ParamInvalid(LightString),
+    #[error("令牌无效")]
+    TokenInvalid,
+    #[error("用户未登录")]
+    LoginRequired,
+    #[error("没有权限")]
+    NotAllowed,
+    #[error("访问太频繁")]
+    TooFrequent,
+    #[error("文件上传请求格式不正确")]
+    MultipartRequired,
+    #[error("未定义的枚举值,{0}")]
+    UndefinedEnumValue(LightString),
+    #[error("没有数据库连接")]
+    NoDbClient,
+    #[error("预编译sql失败,{0}")]
+    PrepareStatementError(anyhow::Error),
+    #[error("查询数据失败,{0}")]
+    QueryError(anyhow::Error),
+    #[error("数据操作失败,{0}")]
+    ExecuteError(anyhow::Error),
+    #[error("开启数据库事务失败,{0}")]
+    OpenTransactionError(anyhow::Error),
+    #[error("获取数据字段失败,{0}")]
+    ExtractDataError(anyhow::Error),
+    #[error("提交数据库事务失败,{0}")]
+    CommitTransactionError(anyhow::Error),
+    #[error("没有缓存连接")]
+    NoCacheClient,
+    #[error("缓存操作失败,{0}")]
+    CacheOperationError(anyhow::Error),
+}
+
+impl ErrNo {
+    pub fn code(&self) -> i32 {
+        return match *self {
+            ErrNo::CommonError(_) => -1,
+            ErrNo::Other(_) => -2,
+            ErrNo::ConfigError(_) => -3,
+            ErrNo::NoService(_) => -4,
+            ErrNo::ServiceBusy(_) => -5,
+            ErrNo::ServicePaused => -6,
+            ErrNo::NoSuchApi => -7,
+            ErrNo::SerializeError(_) => -8,
+            ErrNo::DeserializeError(_) => -9,
+            ErrNo::ApiError(_) => -10,
+            ErrNo::Utf8Only => -11,
+            ErrNo::ParamFormatError => -12,
+            ErrNo::ParamInvalid(_) => -13,
+            ErrNo::TokenInvalid => -14,
+            ErrNo::LoginRequired => -15,
+            ErrNo::NotAllowed => -16,
+            ErrNo::TooFrequent => -17,
+            ErrNo::MultipartRequired => -18,
+            ErrNo::UndefinedEnumValue(_) => -19,
+            ErrNo::NoDbClient => -20,
+            ErrNo::PrepareStatementError(_) => -21,
+            ErrNo::QueryError(_) => -22,
+            ErrNo::ExecuteError(_) => -23,
+            ErrNo::OpenTransactionError(_) => -24,
+            ErrNo::ExtractDataError(_) => -25,
+            ErrNo::CommitTransactionError(_) => -26,
+            ErrNo::NoCacheClient => -27,
+            ErrNo::CacheOperationError(_) => -28,
+        };
+    }
+    pub fn message(&self) -> LightString {
+        return self.to_string().into();
+    }
+}
+
+pub fn open_transaction_error<E>(error: E) -> ErrNo
+where
+    E: std::error::Error + Send + Sync + 'static,
+{
+    ErrNo::OpenTransactionError(error.into())
+}
+
+pub fn prepare_statement_error<E>(error: E) -> ErrNo
+where
+    E: std::error::Error + Send + Sync + 'static,
+{
+    ErrNo::PrepareStatementError(error.into())
+}
+
+pub fn query_error<E>(error: E) -> ErrNo
+where
+    E: std::error::Error + Send + Sync + 'static,
+{
+    ErrNo::QueryError(error.into())
+}
+
+pub fn undefined_enum_value(err_msg: LightString) -> ErrNo {
+    ErrNo::UndefinedEnumValue(err_msg)
+}
+
+pub fn extract_data_error<E>(error: E) -> ErrNo
+where
+    E: std::error::Error + Send + Sync + 'static,
+{
+    ErrNo::ExtractDataError(error.into())
+}
+
+pub fn execute_error<E>(error: E) -> ErrNo
+where
+    E: std::error::Error + Send + Sync + 'static,
+{
+    ErrNo::ExecuteError(error.into())
+}
+
+pub fn commit_transaction_error<E>(error: E) -> ErrNo
+where
+    E: std::error::Error + Send + Sync + 'static,
+{
+    ErrNo::CommitTransactionError(error.into())
+}
--- tihu-native/src/lib.rs
+++ tihu-native/src/lib.rs
@@ -1,6 +1,8 @@
+pub mod errno;
 pub mod http;
 pub mod xml;
 pub use anyhow;
 pub use bytes;
+pub use errno::ErrNo;
 pub use hyper;
 pub use tihu;
