5209e3c913d628b6bcef390c03fab1e84cbff699
omega <omega.xiao@loosegoose.onaliyun.com>
2024-12-09 08:30:54 +0800

    use UUID as Id

--- tihu/src/id.rs
+++ tihu/src/id.rs
@@ -3,23 +3,24 @@ use std::fmt;
 use std::num::ParseIntError;
 use std::ops::Deref;
 use std::str::FromStr;
+use uuid::Uuid;
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Default, Hash)]
-pub struct Id(pub i64);
+pub struct Id8(pub i64);
 
-impl fmt::Display for Id {
+impl fmt::Display for Id8 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self.0)
     }
 }
 
-impl PartialEq<i64> for Id {
+impl PartialEq<i64> for Id8 {
     fn eq(&self, other: &i64) -> bool {
         self.0 == *other
     }
 }
 
-impl Serialize for Id {
+impl Serialize for Id8 {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
@@ -29,55 +30,57 @@ impl Serialize for Id {
     }
 }
 
-impl<'de> Deserialize<'de> for Id {
+impl<'de> Deserialize<'de> for Id8 {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where
         D: Deserializer<'de>,
     {
         let s = String::deserialize(deserializer)?;
         i64::from_str_radix(&s, 10)
-            .map(Id)
+            .map(Id8)
             .map_err(serde::de::Error::custom)
     }
 }
 
-impl From<i64> for Id {
+impl From<i64> for Id8 {
     fn from(id: i64) -> Self {
-        Id(id)
+        Id8(id)
     }
 }
 
-impl Into<i64> for Id {
+impl Into<i64> for Id8 {
     fn into(self) -> i64 {
         self.0
     }
 }
 
-impl Deref for Id {
+impl Deref for Id8 {
     type Target = i64;
     fn deref(&self) -> &Self::Target {
         &self.0
     }
 }
 
-impl AsRef<i64> for Id {
+impl AsRef<i64> for Id8 {
     fn as_ref(&self) -> &i64 {
         &self.0
     }
 }
 
-impl AsMut<i64> for Id {
+impl AsMut<i64> for Id8 {
     fn as_mut(&mut self) -> &mut i64 {
         &mut self.0
     }
 }
 
-impl FromStr for Id {
+impl FromStr for Id8 {
     type Err = ParseIntError;
     fn from_str(src: &str) -> Result<Self, ParseIntError> {
         i64::from_str(src).map(From::from)
     }
 }
+
+pub type Id = Uuid;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct PrimaryKey {
